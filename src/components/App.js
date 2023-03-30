4a9384fb455a60ef2935ffe7039d9fabfe8ba979
Alper Yazagan <alperyazagan@gmail.com>
2023-03-30 18:35:34 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="nav">I'm a link!</a>
     </nav>
   );
 }
@@ -16,15 +16,16 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return <h1 id="about">About</h1>;
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
